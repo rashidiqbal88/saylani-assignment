2cd116d0ed252a487ff4e035a711d2f6d3c1532e
rashidiqbal88 <rashidiqbal.developer@gmail.com>
2024-01-01 23:30:38 +0500

    List

--- main.dart
+++ main.dart
@@ -1,4 +1,28 @@
 void main() {
-  print("Configure Github");
-  print("Github configuration sucssfully...");
+  List<String> student = ["Ali", "Rashid", "Bilal", "Noman", "Aqib"];
+  print(student[0]);
+
+  print(student.length);
+
+//  print(student.indexOf("Noman"));
+//  print(student.length - 1);
+
+//ADD
+  student.add("kashif");
+  print(student);
+
+//INSERT POSITION
+  student.insert(1, "obaid");
+  print(student);
+
+//REMOVE By NAme
+  student.remove("Rashid");
+  print(student);
+
+//REMOVE By Index
+  student.removeAt(2);
+  print(student);
+
+  // List<num> marks = [34, 40, 50, 25];
+  // print(marks);
 }
